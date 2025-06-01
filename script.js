7ee8cdaf13eb33da5b1aa80820354e5812f78cdb
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-06-01 17:12:10 -0400

    Began work on gameboard factory function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // JS for tic tac toe
+
+const gameboard = (function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = 0;
+        }
+    }
+
+    function getBoard() {
+        return board;
+    }
+
+    function makeMove() {
+
+    }
+
+    return {
+        getBoard,
+    }
+})();
